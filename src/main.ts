a8c1ad0b285f107cfc4e7ff4d596d6822f1b4a67
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-05-10 14:06:59 +0200

    refactor: persist pointer id in localstorage

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,21 @@ import './style.css';
 const pb = new PocketBase('http://127.0.0.1:8090');
 const pm = new PointerManager('pointers', 'pointer');
 
-// create a pointer record for this user
-const myPointer = await pb.collection('visitors').create({});
-let myPointerId = myPointer.id;
+async function getPointerId() {
+  let id = localStorage.getItem('myPointerId');
+  if(id !== 'null' && id !== null) {
+    // TODO: verify record exists on server
+    return id;
+  }
+
+  const myPointer = await pb.collection('visitors').create({});
+  localStorage.setItem('myPointerId', myPointer.id);
+  return myPointer.id
+}
+
+// obtain a pointer id
+let myPointerId = await getPointerId()
+console.log('pointer id:', myPointerId);
 
 // create pointers for all connected clients (including this one)
 const allPointers = await pb.collection('visitors').getFullList()
@@ -18,13 +30,11 @@ allPointers.forEach(pointer => {
 
 // register listener to provide updates to other clients
 window.addEventListener('mousemove', (e) => {
-  if (myPointerId !== '') {
-    pm.updatePointer(myPointerId, e.clientX, e.clientY)
-    pb.collection('visitors').update(myPointerId, {
-      x: e.clientX,
-      y: e.clientY
-    });
-  }
+  pm.updatePointer(myPointerId, e.clientX, e.clientY)
+  pb.collection('visitors').update(myPointerId, {
+    x: e.clientX,
+    y: e.clientY
+  });
 })
 
 // subscribe to updates from other clients
